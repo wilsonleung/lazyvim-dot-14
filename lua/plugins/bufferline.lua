7522e0c11503c91dd0ae9646f54353d3598ca079
Wilson Leung <wilsonpsleung@gmail.com>
2025-04-11 14:11:05 +0800

    change tab highlight with icon

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,10 +2,10 @@ return {
   {
     "akinsho/bufferline.nvim",
     opts = {
-      highlights = {
-        buffer_selected = {
-          underline = true,
-          bold = true, -- optional, makes text bold
+      options = {
+        indicator = {
+          style = "icon",
+          icon = " ",
         },
       },
     },
