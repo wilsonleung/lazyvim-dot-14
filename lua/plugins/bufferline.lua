017c02839f2ead526e13ba4b7176fc51030e5f4b
Wilson Leung <wilsonpsleung@gmail.com>
2025-04-11 13:41:43 +0800

    add underline to selected buffer

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      highlights = {
+        buffer_selected = {
+          underline = true,
+          bold = true, -- optional, makes text bold
+        },
+      },
+    },
+  },
+}
